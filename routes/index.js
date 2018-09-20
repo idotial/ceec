e703fad6598db62186209c3db55e8abe677a4490
homeMac <724104612@qq.com>
2018-09-20 10:33:18 +0800

    add route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ppp', function(req, res, next) {
+  res.render('index', { title: 'ppp' });
+});
+
+router.get('/ttt', function(req, res, next) {
+  res.render('index', { title: 'ttt' });
+});
+
 module.exports = router;
+// http://mp.weixin.qq.com/profile?src=3&timestamp=1537326619&ver=1&
+// signature=MBfngB*O3oKjHSnO*gFtqp80ZS4kDlgCFMAnF*SuE79aHzcAN9wFoQu03qzgfTBPjS9vn1NkKrInsVaid2pAfA==
+// https://mp.weixin.qq.com/profile?src=3&timestamp=1537326619&ver=1&
+// signature=Lf5Pc7S*l2RxK34Yedd2yXQCtl3przKh2xxnySJ-jb95DldtE2zT9ApDA4mqnLdmp5u-8IZITjHG7SRjXAZeAw==
